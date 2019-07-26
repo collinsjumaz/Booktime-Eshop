b73f2f04a7460edbf402137cdf4c0a4c1ad0068b
COLLINS JUMA <collinsjumaz42@gmail.com>
2019-07-26 13:50:10 +0300

    Changed login Model

--- booktime/settings.py
+++ booktime/settings.py
@@ -129,4 +129,4 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-
+AUTH_USER_MODEL = "main.User"
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.shortcuts import get_object_or_404
+from main import models
+from main import views
+
 
 urlpatterns = [
     path('about-us/',TemplateView.as_view(template_name="about_us.html")),
     path( "",TemplateView.as_view(template_name="home.html")),
+    path("product/<slug:slug>/", DetailView.as_view(model=models.Product), name="product",),
 ]
